37b4ac1d09e555c59c6f4c4b2e37616328f41034
Dusan Jeftic <dusan.jeftic@outlook.com>
2018-03-05 12:28:19 +0100

    React realtime graphs

---
+++ LiveCharttt/src/app.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import LiveChart from 'LiveChart.jsx';
+import HelloWorld from 'HelloWorld.jsx';
+
+ReactDOM.render(
+    <div><LiveChart /> <HelloWorld /></div>,
+    document.getElementById('Content')
+);
